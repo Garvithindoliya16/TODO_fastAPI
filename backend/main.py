8dc358fd69bfefbeb5200280bd8504023a99b84c
garvithindoliya16 <garvithindoliya16@gmail.com>
2026-05-23 11:01:00 +0530

    Fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
+
 from fastapi.middleware.cors import CORSMiddleware
-from database.connection import engine, Base
+
 from routers.todo_router import router as todo_router
 from routers.auth_router import router as auth_router
-from models import user_model
-from models import todo_model
+
 
 app = FastAPI()
 
@@ -25,4 +25,5 @@ app.add_middleware(
 
 
 app.include_router(todo_router)
+
 app.include_router(auth_router)
