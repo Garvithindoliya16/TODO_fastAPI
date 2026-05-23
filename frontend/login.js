b7ec075af68e6baefe9dd555eff1a14f5cb49b8c
garvithindoliya16 <garvithindoliya16@gmail.com>
2026-05-23 10:31:50 +0530

    Updated production API URLs

--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,5 @@
-const API_URL = "http://127.0.0.1:8000"
+const API_URL = "https://todo-fastapi-6u5x.onrender.com"
+
 
 
 async function login(){
--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000"
+const API_URL = "https://todo-fastapi-6u5x.onrender.com"
 
 const token = localStorage.getItem("token")
 
